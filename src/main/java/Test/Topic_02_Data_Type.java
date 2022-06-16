aef65e4032c523709800a367624a940c46b2160c
chiDL <chidl@unstatic.co>
2022-06-16 19:48:55 +0700

    update java topic 02 data type

---
+++ src/main/java/Test/Topic_02_Data_Type.java
@@ -0,0 +1,23 @@
+package Test;
+
+public class Topic_02_Data_Type {
+    int age;
+    String address = "ngo cho Kham Thien";
+
+    public static void main(String[] args) {
+//        String name = "Chidl";
+//        Topic_02_Data_Type topic02 = new Topic_02_Data_Type();
+//        System.out.println(topic02.age);
+//        System.out.println(address);
+//        System.out.println(name);
+//
+        int x = 5;
+        int y = x;
+        System.out.println(x);
+        System.out.println(y);
+
+        y = 10;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
