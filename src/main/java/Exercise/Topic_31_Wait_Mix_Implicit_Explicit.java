e4dddb6df4e663c1f253b5c5748362d0d15583ee
ChiDL96 <chidl6996@gmail.com>
2022-07-07 18:22:59 +0700

    topic 31 wait mix implicit explicit

---
+++ src/main/java/Exercise/Topic_31_Wait_Mix_Implicit_Explicit.java
@@ -0,0 +1,80 @@
+package Exercise;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_31_Wait_Mix_Implicit_Explicit {
+    String projectPath = System.getProperty("user.dir");
+    WebDriver driver;
+    WebDriverWait explicitWait;
+
+    @BeforeClass
+    public void beforeClass() {
+        System.setProperty("webdriver.chrome.driver", projectPath + "/BrowserDrivers/chromedriver");
+        driver = new ChromeDriver();
+
+    }
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    @Test
+    public void TC_01_Element_Found(){
+        driver.get("https://www.facebook.com/");
+        By emailIDBy = By.id("email");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        System.out.println("Start implicit: " + getTimeNow());
+        Assert.assertTrue(driver.findElement(emailIDBy).isDisplayed());
+        System.out.println("End implicit: " + getTimeNow());
+
+
+        explicitWait = new WebDriverWait(driver, 15);
+        System.out.println("Start implicit: " + getTimeNow());
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(emailIDBy));
+        System.out.println("End implicit: " + getTimeNow());
+    }
+
+    @Test
+    public void TC_02_Element_Not_Found_Only_Implicit(){
+        driver.get("https://www.facebook.com/");
+        By fakeIDBy = By.id("fake");
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        System.out.println("Start implicit: " + getTimeNow());
+        Assert.assertTrue(driver.findElement(fakeIDBy).isDisplayed());
+        System.out.println("End implicit: " + getTimeNow());
+    }
+
+    @Test
+    public void TC_03_Element_Not_Found_Only_Explicit(){
+        driver.get("https://www.facebook.com/");
+        By fakeIDBy1 = By.id("fake1");
+        explicitWait = new WebDriverWait(driver, 15);
+        System.out.println("Start implicit: " + getTimeNow());
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(fakeIDBy1));
+        System.out.println("End implicit: " + getTimeNow());
+    }
+
+    @Test
+    public void TC_04_Element_Not_Found_Mix(){
+        //implicir < explicit
+        //implicir = explicit
+        //implicir > explicit
+
+    }
+
+    public String getTimeNow(){
+        Date date = new Date();
+        return date.toString();
+    }
+}
