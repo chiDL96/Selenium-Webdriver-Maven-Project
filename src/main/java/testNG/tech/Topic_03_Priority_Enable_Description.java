1dd6748af0b7c87b702fed2f805866dd552c1c9b
chiDL <chidl@unstatic.co>
2022-07-28 23:00:35 +0700

    topic 03 priority, enable, description

--- src/main/java/testNG/tech/Topic_03_Priority_Enable_Description.java
+++ src/main/java/testNG/tech/Topic_03_Priority_Enable_Description.java
@@ -1,4 +1,20 @@
 package testNG.tech;
 
+import org.testng.annotations.Test;
+
 public class Topic_03_Priority_Enable_Description {
+    @Test(priority = 1, enabled = false)
+    public void B() {
+        System.out.println("B");
+    }
+
+    @Test(description = "regression test")
+    public void A() {
+        System.out.println("A");
+    }
+
+    @Test(priority = 3)
+    public void C() {
+        System.out.println("C");
+    }
 }
