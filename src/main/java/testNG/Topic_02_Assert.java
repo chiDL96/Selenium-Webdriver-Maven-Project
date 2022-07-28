720110b7b035daea1d5e39ac43ede6fbcf6fbb39
chiDL <chidl@unstatic.co>
2022-07-28 21:54:39 +0700

    topic 02 assert

--- src/main/java/testNG/Topic_02_Assert.java
+++ src/main/java/testNG/Topic_02_Assert.java
@@ -1,2 +1,18 @@
-package testNG;public class Topic_02_Assert {
+package testNG;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Topic_02_Assert {
+    //Thu vien assert: Kiem tra tinh dung dan cua du lieu
+    //Mong doi no dung/ sai/ dau vao va dau ra nhu nhau
+    //Bang null/ khac null
+    @Test
+    public void TC_01() {
+        String addressCity = "Ho Chi Minh city";
+
+        Assert.assertTrue(addressCity.contains("city"));
+        Assert.assertFalse(addressCity.contains("Ha Noi"));
+        Assert.assertEquals(addressCity, "Ho Chi Minh city");
+    }
 }
