6951fc2b44deb1143adde40d8dfddeadb1f79185
chiDL <chidl@unstatic.co>
2022-07-29 23:35:55 +0700

    topic 05 multiple browser

--- src/main/java/testNG/tech/Topic_05_Multiple_Browser.java
+++ src/main/java/testNG/tech/Topic_05_Multiple_Browser.java
@@ -1,4 +1,70 @@
 package testNG.tech;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
+
 public class Topic_05_Multiple_Browser {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+
+    By emailTextbox = By.xpath("//*[@id='email']");
+    By passwordTextbox = By.xpath("//*[@id='pass']");
+    By loginButton = By.xpath("//*[@id='send2']");
+
+    @Parameters("browser")
+    @BeforeClass
+    public void beforeClass(String browserName) {
+        switch (browserName) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", projectPath + "/BrowserDrivers/chromedriver");
+                driver = new ChromeDriver();
+                break;
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", projectPath + "/BrowserDrivers/geckodriver");
+                driver = new FirefoxDriver();
+                break;
+            case "edge":
+                System.setProperty("webdriver.edge.driver", projectPath + "/BrowserDrivers/msedgedriver");
+                driver = new FirefoxDriver();
+                break;
+            default:
+                System.out.println("Browser name is not valid");
+                break;
+        }
+
+
+    }
+
+    @Test(dataProvider = "user_pass")
+    public void TC_01_Login_To_System(String username, String password) throws InterruptedException {
+        driver.get("http://live.techpanda.org/index.php/customer/account/login/");
+
+        driver.findElement(emailTextbox).sendKeys(username);
+        driver.findElement(passwordTextbox).sendKeys(password);
+        driver.findElement(loginButton).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@class='col-1']//p")).getText().contains(username));
+        driver.findElement(By.xpath("//header[@id='header']//span[text()='Account']")).click();
+
+        driver.findElement(By.xpath("//a[text()='Log Out']")).click();
+    }
+
+    @DataProvider(name = "user_pass")
+    public Object[][] UserAndPasswordData() {
+        return new Object[][]{
+                {"selenium_11_01@gmail.com", "111111"},
+                {"selenium_11_02@gmail.com", "111111"},
+                {"selenium_11_03@gmail.com", "111111"}
+        };
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.close();
+    }
 }
